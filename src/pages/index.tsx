ed4f0c836fba7e3e47a3c1b7b71458a0a1bdcd07
unknown <prathamjagga123@gmail.com>
2023-09-10 16:52:31 +0530

    check workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Container } from '@/styles/HomeStyle'
 const Home: NextPage = () => {
   return (
     <Container>
-      <h1>Hello, Techdome!</h1>
+      <h1>Hello, Techdome Again!</h1>
     </Container>
   )
 }
